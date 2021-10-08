fcd47da99350a185e7fa3e66b1d0ae862feeaa48
匡俊东 <70204596+233-kuang@users.noreply.github.com>
2021-10-08 22:26:37 +0800

    Add files via upload

---
+++ for循环.py
@@ -0,0 +1,13 @@
+# for i in 'hello':
+#     #一次循环是一次字符串的一个字符
+#     print(i)
+
+#range(n)  会生成0~n的数据不包含n
+# for i in range(5):
+#     print(i)
+#range（a~b） 会生成 a~b范围的数据不包含b
+# for i in range(6 , 9):
+#     print(i)
+#range（a~b step） 会生成 a~b范围的数据 每个数据的间隔（步长（step））
+for i in range(1 , 7 , 3):
+    print(i)
