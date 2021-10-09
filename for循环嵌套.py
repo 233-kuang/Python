fc65c0ecadca2f396348b273ce375b6311cdc4e3
匡俊东 <70204596+233-kuang@users.noreply.github.com>
2021-10-09 22:33:54 +0800

    Add files via upload

---
+++ for循环嵌套.py
@@ -0,0 +1,7 @@
+#操场跑5圈
+for i in range(5):
+    print('操场跑圈')
+    # 跑一圈做3个俯卧撑
+    for j in range(3):
+        print('俯卧撑')
+
---
+++ 使用循环打印正方形.py
@@ -0,0 +1,13 @@
+#i = int(input('请输入一行有多少星：'))
+
+
+j = 5
+k = 0
+while k < j:
+    i = 0
+    while i < j:
+        print('*', end=' ')
+        i += 1
+    print()
+    k += 1
+
